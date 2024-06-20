de640f987d7a060d03c7a34bf298875b0bd54889
shadowofcat <catmanariel@gmail.com>
2024-06-20 11:43:38 +0300

    shockwave hammer

--- mobile game/Assets/hammer system/effects/ShockWaveEffect.cs
+++ mobile game/Assets/hammer system/effects/ShockWaveEffect.cs
@@ -12,6 +12,10 @@ public class ShockWaveEffect : HammerEffect
     public float OffsetBetweenSpawns;
     public float TimeBetweenSpawns;
 
+    public int SpikeAmount;
+
+    public GameObject Spike;
+
 
     public override void ApplyEffect(Enemy Target)
     {
@@ -22,12 +26,38 @@ public class ShockWaveEffect : HammerEffect
     {
         if(hammer.HitCounter % 3 == 0)
         {
-            hammer.StartCoroutine(ShockWave());
+            hammer.StartCoroutine(ShockWave(hammer));
         }
     }
 
-    public IEnumerator ShockWave()
+    public IEnumerator ShockWave(Hammer hammer)
     {
         yield return null;
+
+        Vector2 startPos  = new Vector2(hammer.transform.position.x + (hammer.isHammeringRight ? spawnOffsetX : -spawnOffsetX), spawnY);
+
+        List<GameObject> parents = new List<GameObject>();
+
+        for (int i = 0; i < SpikeAmount; i++)
+        {
+            var parent = new GameObject("spike parent");
+
+            parents.Add(parent);
+
+            var spike = Instantiate(Spike, parent.transform);
+
+            parent.transform.position = startPos;
+
+            startPos += new Vector2(hammer.isHammeringRight ? OffsetBetweenSpawns : -OffsetBetweenSpawns, 0);
+
+            yield return new WaitForSeconds(TimeBetweenSpawns);
+        }
+
+        yield return new WaitForSeconds(1);
+
+        foreach (var parent in parents)
+        {
+            Destroy(parent);
+        }
     }
 }
